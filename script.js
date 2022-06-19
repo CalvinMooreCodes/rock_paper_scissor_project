f58e5c83546f14b6c0d4cd27640497b991e916c8
CalvinMooreCodes <calvin.moore31@outlook.com>
2022-06-19 12:00:53 +0200

    added random computer move + added non-case sensitive player move functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+function computerPlay() {
+    let randomNumber = Math.random();
+    let computerChoice;
+
+    if(randomNumber <= 0.33) {
+        computerChoice = 'rock';
+    } else if(randomNumber <= 0.66) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissor';
+    }
+}
+
+function playerPlay() {
+    let playerChoice = prompt('choose between rock, paper and scissor');
+
+    //making playerChoice non-case sensitive
+    let capitalLetter = playerChoice.charAt(0).toUpperCase();
+    let stringRemainder = playerChoice.slice(1);
+    playerChoice = capitalLetter.concat(stringRemainder); 
+
+    if(playerChoice !== "Rock" && playerChoice !== "Paper" && playerChoice !== "Scissor") {
+        alert("You didnt choose between rock, paper and scissor, try again.");
+        playerChoice = "";
+        return
+    }
+}
