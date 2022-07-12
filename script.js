460df6b7663216b083589ac32e5eda9fe7a20347
CalvinMooreCodes <calvin.moore31@outlook.com>
2022-07-12 21:20:44 +0200

    added score tracker and notifies player if somebody has reached 5 won rounds

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+let playerScore = 0;
+let computerScore = 0;
 
 const rockBtn = document.createElement('button');
 const paperBtn = document.createElement('button');
 const scissorBtn = document.createElement('button');
-const div = document.createElement('div');
+const divPlayerChoice = document.createElement('div');
+const divComputerChoice = document.createElement('div');
+const divResult = document.createElement('div');
+const divScore = document.createElement('div');
 
 const body = document.querySelector('body');
 
@@ -10,31 +15,40 @@ const body = document.querySelector('body');
 body.appendChild(rockBtn);
 body.appendChild(paperBtn);
 body.appendChild(scissorBtn);
-body.appendChild(div);
+body.appendChild(divPlayerChoice);
+body.appendChild(divComputerChoice);
+body.appendChild(divResult);
+body.appendChild(divScore);
 
 rockBtn.addEventListener('click', () => {
     playerSelection = 'Rock';
     computerPlay();
-    playRound();
-    div.textContent = compareSelections();
+    divPlayerChoice.textContent = `Player chose ${playerSelection}`;
+    divComputerChoice.textContent = `Computer chose ${computerSelection}`;
+    divResult.textContent = compareSelections();
+    divScore.textContent = `Player ${playerScore} : ${computerScore} Computer`;
+    checkForWinner();
 });
 
 paperBtn.addEventListener('click', () => {
     playerSelection = 'Paper';
     computerPlay();
-    playRound();
-    div.textContent = compareSelections();
+    divPlayerChoice.textContent = `Player chose ${playerSelection}`;
+    divComputerChoice.textContent = `Computer chose ${computerSelection}`;
+    divResult.textContent = compareSelections();
+    divScore.textContent = `Player ${playerScore} : ${computerScore} Computer`;
+    checkForWinner();
 });
 
 scissorBtn.addEventListener('click', () => {
     playerSelection = 'Scissor';
     computerPlay();
-    playRound();
-    div.textContent = compareSelections();
+    divPlayerChoice.textContent = `Player chose ${playerSelection}`;
+    divComputerChoice.textContent = `Computer chose ${computerSelection}`;
+    divResult.textContent = compareSelections();
+    divScore.textContent = `Player ${playerScore} : ${computerScore} Computer`;
+    checkForWinner();
 });
-
-
-
 
 // computer chooses one of following string values randomly : Rock, Paper, Scissor. 
 function computerPlay() {
@@ -56,7 +70,6 @@ function compareSelections() {
 
     let winnerRound;
 
-
     // all tie scenarios
     if(playerSelection === computerSelection) {
         winnerRound = "It's a tie! nobody won"; 
@@ -65,36 +78,37 @@ function compareSelections() {
     // all player wins scenarios
     else if(playerSelection == "Rock" && computerSelection == "Scissor") {
         winnerRound = `Player won! ${playerSelection} beats ${computerSelection}.`;
+        playerScore++;
        return winnerRound; 
     } else if(playerSelection == "Paper" && computerSelection == "Rock") {
         winnerRound = `Player won! ${playerSelection} beats ${computerSelection}.`;
+        playerScore++;
         return winnerRound;
     } else if(playerSelection == "Scissor" && computerSelection == "Paper") {
         winnerRound = `Player won! ${playerSelection} beats ${computerSelection}.`;
+        playerScore++;
         return winnerRound;
     } 
     // all computer wins scenarios
     else if(playerSelection == "Scissor" && computerSelection == "Rock") {
         winnerRound = `Computer won! ${computerSelection} beats ${playerSelection}.`;
+        computerScore++;
        return winnerRound; 
     } else if(playerSelection == "Rock" && computerSelection == "Paper") {
         winnerRound = `Computer won! ${computerSelection} beats ${playerSelection}.`;
+        computerScore++;
         return winnerRound;
     } else if(playerSelection == "Paper" && computerSelection == "Scissor") {
        winnerRound = `Computer won! ${computerSelection} beats ${playerSelection}.`;
+       computerScore++;
        return winnerRound;
     }
 }
 
-//plays one single round 
-function playRound() {
-    compareSelections();
+function checkForWinner() {
+    if(playerScore === 5) {
+        alert('Player was first to reach 5 round wins so he won the game!');
+    } else if(computerScore === 5) {
+        alert('Computer was first to reach 5 round wins so he won the game!');
+    }
 }
-
-// //plays 5 rounds
-// function game() {
-//     for(i = 0; i< 5; i++) {
-//         playRound();
-//     }
-// }
-
